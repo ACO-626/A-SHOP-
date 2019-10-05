b83645659a71b74f37fb68f8150fe05be0a3e023
ACO <54153297+ACO-626@users.noreply.github.com>
2019-10-04 19:34:10 -0500

    Add files via upload

--- MAIN2.0.cpp
+++ MAIN2.0.cpp
@@ -12,22 +12,30 @@
 //Declaramos estructura de dato Empleado
 typedef struct _empleado
 {
-	struct Empleado *anterior;
+	struct _empleado *anterior;
 	char nombre[30];
 	char usuario[11];
 	char passwd[11];
 	char area[20];
 	int sueldo;
-	struct Empleado *siguiente;
+	struct _empleado *siguiente;
 };
 typedef struct _empleado EMP;
 
 EMP *WALKER=NULL;
 EMP *PRIMERO=NULL;
+EMP *PRIMEROC=NULL;
+EMP *PRIMEROV=NULL;
 EMP *ULTIMO=NULL;
+EMP *ULTIMOC=NULL;
+EMP *ULTIMOV=NULL;
+EMP *DESTRUCTOR=NULL;
 
 void EMPLEADOS ();
 void MENUAD ();
+void MENUC ();
+void MENUV ();
+int INCINERAR(EMP *PRIM, EMP *LAST, int e);
 int BUSQUEDA (EMP *PRIM);
 int main()
 
@@ -39,6 +47,9 @@ int main()
 	int nec=0; //contador de empleados de compras
 	int nev=0; //contador de area de ventas
 	int A=0;  //Para el switch de menuad
+	int AV=0; //Para el menu de ventas
+	int AC=0; //Para menu de compras
+	int muerte=0;
 	do
 	{
 		EMPLEADOS();
@@ -46,21 +57,46 @@ int main()
 		switch(o)
 		{
 			case 1:
-				if(nec==0)
+				if(nev==0)
 				{
-					printf("\nAun no hay empleados del area de compra registrados, dirigete a Administrador para agregarlos\n");
+					printf("\nAun no hay empleados del area de venta registrados, dirigete a Administrador para agregarlos\n");
 					getchar();
 				}
 				else
 				{
-					//Aqui va el menu de empleados
+					if(BUSQUEDA(PRIMEROV)==1)
+					{
+						//Iniciado sesión
+						do
+						{
+							MENUV ();
+							scanf("%i",&AV);
+							switch(AV)
+							{
+								case 1://cambiar producto
+								break;
+								case 2:
+								break;
+								default:
+								printf("\nRespuesta invalida\n");
+								getchar();
+								break;
+							}
+						}while(AV!=2);
+						
+					}
+					else
+					{
+					}
+					
+					
 				}
 			break;
 		    case 2:
 
-		    	if(nev==0)
+		    	if(nec==0)
 				{
-		    	printf("\nAun no hay empleados del area de ventas registrados, dirigete a Administrador para agregarlos\n");
+		    	printf("\nAun no hay empleados del area de compras registrados, dirigete a Administrador para agregarlos\n");
 		    	getchar();
 				}
 				else
@@ -129,9 +165,125 @@ int main()
 							break;
 							case 6:
 								//Agregar empleado
+								EMP *emp;
+								emp=(EMP *)malloc(sizeof(EMP));
+								printf("\nIngrese el nombre del nuevo empleado: ");
+								fflush(stdin);
+								gets(emp->nombre);
+								printf("\nIngrese el nombre del usuari: ");
+								printf("\nal menos debe tener uno, sin espacios y el sistema es sensible a mayusculas y minusculas\n");
+								printf("Nombre de usuario: ");
+								fflush(stdin);
+								gets(emp->usuario);
+							    printf("\nAhora su contrasena debera tener al menos un caracter como mínimo y 10 como maximo, no se aceptan espacios\n ");
+					            printf("no se aceptan caracteres especiales ni acentos\n\n");
+					            printf("Contraseña:");
+					            fflush(stdin);
+					            gets(emp->passwd);
+					            printf("\nAhora debemos de registrar por especificaciones del sistema su salario\n");
+								printf("Salario: ");
+								fflush(stdin);
+								scanf("%i",&emp->sueldo);
+								printf("\n Por ultimo debes registrar su area de trabajo solo puede ser compra (C) o ventas(V)\n ");
+								printf("Area C para compras V para ventas: ");
+								fflush(stdin);
+								gets(emp->area);
+								if(strcmp(emp->area,"C")==0)
+								{
+									if(nec==0)
+									{
+									emp->anterior=NULL;
+									emp->siguiente=NULL;
+									PRIMEROC=emp;
+									ULTIMOC=emp;
+									nec++;
+									}
+									else
+									{
+										emp->anterior=ULTIMOC;
+										emp->siguiente=NULL;
+										ULTIMOC->siguiente=emp;
+										ULTIMOC=emp;
+									 nec++;
+									}
+								
+								}
+								else
+								{
+									if(strcmp(emp->area,"V")==0)
+									{
+									if(nev==0)
+									{
+									emp->anterior=NULL;
+									emp->siguiente=NULL;
+									PRIMEROV=emp;
+									ULTIMOV=emp;
+									nev++;
+									}
+									else
+									{
+										emp->anterior=ULTIMOV;
+										emp->siguiente=NULL;
+										ULTIMOV->siguiente=emp;
+										ULTIMOV=emp;
+									    nev++;
+									}
+									}
+									else
+									{
+									
+									printf("\nArea no valida, intenta de nuevo\n");
+									getchar();
+									break;
+								    }
+								}
+								
+								
 							break;
 							case 7:
 								//Correr empleado
+								printf("\nDe que area desea eliminar: ");
+								EMPLEADOS ();
+								scanf("%d",&muerte);
+								switch(muerte)
+								{
+									case 1:
+										if(INCINERAR (PRIMEROV, ULTIMOV, nev)==1)
+										{
+											nev--;
+										}
+										else
+										{
+											
+										}
+									break;
+									case 2:
+									if(INCINERAR (PRIMEROC, ULTIMOC, nec)==1)
+										{
+											nec--;
+										}
+										else
+										{
+											
+										}
+									break;
+									case 3:
+										if(INCINERAR (PRIMERO,ULTIMO,ne)==1)
+										{
+											ne--;
+										}
+										else
+										{
+											
+										}
+									break;
+									case 4:
+									break;
+									default:
+									break;
+									
+								}
+								
 							break;
 							case 8:
 							break;	
@@ -174,7 +326,7 @@ void EMPLEADOS ()
 	printf("\nSeleccione su rubro");
 	printf("\n\n1) Empleado de ventas\n");
 	printf("2)Empleado de compras\n");
-	printf("3)Empleado de ventas\n");
+	printf("3)ADMINISTRADOR\n");
 	printf("4)Salir\n");
 	printf("Respuesta:  ");
 		
@@ -225,7 +377,7 @@ void MENUAD ()
 {
 	printf("\nSeleccione una accion\n");
 	printf("\n1) Agregar departamento");
-	printf("\n 2)Dar de baja departamento");
+	printf("\n2)Dar de baja departamento");
 	printf("\n3)Dar de baja articulo");
 	printf("\n4)Dar de alta articulo");
 	printf("\n5)Modificar datos");
@@ -235,9 +387,93 @@ void MENUAD ()
 	printf("\nRespuesta:  ");
 	
 }
+void MENUC ()
+{
+	printf("\n1)Sustituir producto");
+	printf("\n2)Cerrar sesion");
+	printf("\nRespuesta:  ");
+}
 
+void MENUV ()
+{
+	printf("\n1)Producto vendido");
+	printf("\n2)Cerrar sesion");
+	printf("\nRespuesta:  ");
+}
 
-
+int INCINERAR(EMP *PRIM, EMP *LAST, int e)
+{
+	int i=0;
+	char USU[11];
+	if(e==0)
+	{
+		printf("\nNo hay nada que eliminar\n");
+		getchar();
+		return 0;
+	}
+	else
+	{
+		if(e==1)
+		{
+			DESTRUCTOR=PRIM;
+			PRIM=NULL;
+			WALKER=NULL;
+			free(DESTRUCTOR);
+			printf("\nSe eliminara el unico elemento\n");
+			return 1;
+		}
+		else
+		{
+		  do{
+		  		printf("USUARIO A ELIMINAR: ");
+		  		fflush(stdin);
+		  		gets(USU);
+			do
+			{
+				
+				if(strcmp(WALKER->usuario,USU)==0)
+				{
+						if(WALKER->anterior==NULL)
+						{
+						 DESTRUCTOR=WALKER;
+						 WALKER=WALKER->siguiente;
+						 PRIM=WALKER;
+						 free(DESTRUCTOR);
+						 WALKER->anterior=NULL;	
+						}
+						else
+						{
+							if(WALKER->siguiente==NULL)
+							{
+								DESTRUCTOR=WALKER;
+								WALKER=WALKER->anterior;
+								LAST=WALKER;
+								free(DESTRUCTOR);
+								WALKER->siguiente=NULL;	
+							}
+							else
+							{
+								DESTRUCTOR=WALKER;
+								WALKER=WALKER->anterior;
+								WALKER->siguiente=DESTRUCTOR->siguiente;
+								WALKER=WALKER->siguiente;
+								WALKER->anterior=DESTRUCTOR->anterior;
+								free(DESTRUCTOR);
+							}
+						}
+						return 1;	
+				}
+				else
+				{
+				}
+			}while(WALKER->siguiente=NULL);
+			printf("\nNo se encontró el usuario vuelva a intentar\n");
+			i++;
+	       }while(i!=3);
+		}
+	}
+		
+}
 
 
 
